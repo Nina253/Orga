96306510b136564da071046cd9fddd4f3978b0ee
Anthony253400 <mirandanthony2005@yahoo.fr>
2025-10-23 16:38:33 +0200

    navbar

---
+++ navbar.php
@@ -0,0 +1,20 @@
+<?php
+$base = '/ORGA'; 
+?>
+
+
+<nav class="navbar">
+  <div class="nav-gauche">
+    <img src="images/logo.png" alt="logo">
+  </div>
+  
+  <div class="nav-center">
+    <a href="index.php">
+      <span>Orga +</span>
+    </a>
+  </div>
+    
+  <div class="nav-droite">
+
+  </div>
+</nav>
