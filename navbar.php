e9e74a7392088ec2055bb6661496a57d03ac49c4
Anthony253400 <mirandanthony2005@yahoo.fr>
2025-11-05 16:33:54 +0100

    navbar

--- navbar.php
+++ navbar.php
@@ -1,20 +1,29 @@
 <?php
-$base = '/ORGA'; 
+session_start();
+$base = '/Orga'; // ton dossier racine
 ?>
 
-
 <nav class="navbar">
-  <div class="nav-gauche">
-    <img src="images/logo.png" alt="logo">
-  </div>
-  
-  <div class="nav-center">
-    <a href="index.php">
-      <span>Orga +</span>
+  <div class="nav-left">
+    <a href="<?= $base ?>/index.php" class="brand-link">
+      <img src="<?= $base ?>/images/logo.png" alt="Logo" class="logo">
+      
     </a>
   </div>
-    
-  <div class="nav-droite">
 
+  <div class="nav-mid"> 
+      <span class="brand">Orga+</span>
+
+  </div>
+
+  <div class="nav-right">
+    <a href="<?= $base ?>/forum.php" class="nav-link">Forum</a>
+
+    <?php if (isset($_SESSION['client'])): ?>
+      <a href="<?= $base ?>/compte.php" class="btn btn-green">Mon compte</a>
+      <a href="<?= $base ?>/logout.php" class="btn btn-red">Déconnexion</a>
+    <?php else: ?>
+      <a href="<?= $base ?>/login.php" class="btn btn-black">Se connecter</a>
+    <?php endif; ?>
   </div>
 </nav>
