ff6955774f452b6252f265c836489287bdb01efd
LeandreVSP <leandrevsp@gmail.com>
2025-11-07 10:45:25 +0100

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,5 @@
+<?php
+	session_start();
+	$_SESSION["client"]=$_POST["email"];
+ 
+?>
---
+++ deconnexion.php
@@ -0,0 +1,13 @@
+<?php
+	session_start();
+	session_destroy();
+	
+?>
+<html>
+
+<head>
+<?php>	
+	echo '<meta http-equiv="refresh" content="0;home.php"/>';
+?>
+</head>
+</html>
