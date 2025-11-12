f62484e8ca6fa2a79dcb1bc212552d54bc16b52d
LeandreVSP <leandrevsp@gmail.com>
2025-11-12 23:28:30 +0100

    enregistrement.php

--- enregistrement.php
+++ enregistrement.php
@@ -1,19 +1,16 @@
 <?php
 	$mail=$_POST["email"];
-	$num=$_POST["num"];
 	$mdp=$_POST["mdp"];
 	
 	function enregistrer($num, $mail, $mdp) {
 		require 'bd.php';
 		$bdd = getBD();
 		
-		$rep = $bdd->prepare("INSERT INTO utilisateurs (nom,prenom,date_naiss,genre,niveau_educ_parents,num, mail, mdp) VALUES ('','','0001-01-01','','',?,?,?);");
-		
-		$num = (int)$num;
-		$rep -> execute([$num,$mail,$mdp]);
+		$rep = $bdd->prepare("INSERT INTO utilisateurs (nom,prenom,genre,niveau_educ_parents, mail, mdp) VALUES ('','','0001-01-01','','',?,?,?);");
+		$rep -> execute([$mail,$mdp]);
 	}
 	
-	enregistrer($num, $mail, $mdp);
+	enregistrer($mail, $mdp);
 	echo '<meta http-equiv="refresh" content="0;home.php"/>';
 
 
