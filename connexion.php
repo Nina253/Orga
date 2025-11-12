81719649da2723bd8a217b59bf0def8921bde3b7
LeandreVSP <leandrevsp@gmail.com>
2025-11-12 23:13:17 +0100

    connexion.php

--- connexion.php
+++ connexion.php
@@ -1,5 +1,12 @@
 <?php
 	session_start();
-	$_SESSION["client"]=$_POST["email"];
- 
+	$_SESSION["mail"]=$_POST["email"];
+	$_SESSION["mdp"]=$_POST["mdp"];
+	$_SESSION["num"]=$_POST["num"];
+	if(isset($_SESSION['mail'])&&isset($_SESSION['mdp'])){
+			require "bd.php";
+			$bdd=getBD(); 
+			$rep= $bdd->prepare("SELECT * FROM clients WHERE mail = ?");
+ 			$rep->execute([$_SESSION['mail']]);
+
 ?>
