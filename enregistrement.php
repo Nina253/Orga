949554b535676a664c1defb225becc733ceeb198
LeandreVSP <leandrevsp@gmail.com>
2025-11-13 18:40:57 +0100

    enregistrement.php

--- enregistrement.php
+++ enregistrement.php
@@ -2,13 +2,15 @@
 	$mail=$_POST["email"];
 	$mdp=$_POST["mdp"];
 	$num=$_POST["num"];
+
+	$mdpC=password_hash($mdp,PASSWORD_DEFAULT);
 	
 	function enregistrer($mail, $mdp) {
 		require 'bd.php';
 		$bdd = getBD();
 		
 		$rep = $bdd->prepare("INSERT INTO utilisateurs (mail, mdp, num) VALUES (?,?);");
-		$rep -> execute([$mail,$mdp,$num]);
+		$rep -> execute([$mail,$mdpC,$num]);
 	}
 	
 	enregistrer($mail, $mdp);
@@ -17,6 +19,7 @@
 
 
 ?>
+
 
 
 
