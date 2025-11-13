d6ec760f8e40b3419c89d6a5cb4cfddff91452ae
LeandreVSP <leandrevsp@gmail.com>
2025-11-13 23:12:12 +0100

    connexion.php

--- connexion.php
+++ connexion.php
@@ -1,9 +1,13 @@
 <?php
 	session_start();
+
+
 	$_SESSION["mail"]=$_POST["email"];
 	$_SESSION["mdp"]=$_POST["mdp"];
 	$_SESSION["num"]=$_POST["num"];
+
 	if(isset($_SESSION['mail'])&&isset($_SESSION['mdp'])){
+		
 			require "bd.php";
 			$bdd=getBD(); 
 			$rep= $bdd->prepare("SELECT * FROM utilisateurs WHERE mail = ?");
@@ -23,5 +27,6 @@
 			die;
 	}
 ?>
+
 
 
