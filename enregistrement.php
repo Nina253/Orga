a44dffd2cef14fc180d76b26698c394991a779da
LeandreVSP <leandrevsp@gmail.com>
2025-11-13 18:14:07 +0100

    enregistrement.php

--- enregistrement.php
+++ enregistrement.php
@@ -6,7 +6,7 @@
 		require 'bd.php';
 		$bdd = getBD();
 		
-		$rep = $bdd->prepare("INSERT INTO etudiant (id_etud,nom,prenom,genre,niveau_educ_parents, mail, mdp) VALUES ('','','','',?,?);");
+		$rep = $bdd->prepare("INSERT INTO utilisateurs (mail, mdp) VALUES (?,?);");
 		$rep -> execute([$mail,$mdp]);
 	}
 	
@@ -16,5 +16,6 @@
 
 
 ?>
+
 
 
