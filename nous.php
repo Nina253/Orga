071419608262f8cab56e6fa7002c26842f608474
Nina <ndavalentin05@gmail.com>
2025-11-14 09:31:16 +0100

    css

---
+++ nous.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+	<link rel="stylesheet" href="styles/styles.css" type="text/css" media="screen" > 
+    <title>Orga+</title>
+</head>
+<body id="nous_page">
+    <h1>Qui sommes-nous ?</h1>
+    <p>
+    Nous sommes une équipe dédiée à aider les étudiants à mieux organiser leur quotidien, améliorer leurs habitudes et atteindre leur plein potentiel.
+    <br><br>
+    L'équipe Orga+ est composée de 4 personnes :
+    <br>
+    <ul>
+        <li><p>• Florient Marchal - 22 ans - florient.marchal@etu.univ-montp3.fr</p></li>
+        <li><p>• Anthony Miranda - 20 ans</p></li>
+        <li><p>• Nina Valentin - 20 ans</p></li>
+        <li><p>• Léandre Vignol - 21 ans</p></li>
+    </ul>
+    </p>
+    
+</body>
+</html>
