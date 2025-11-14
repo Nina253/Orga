9c4dcb47dc533e7e016be2ed07872a37e02d9995
LeandreVSP <leandrevsp@gmail.com>
2025-11-14 09:42:20 +0100

    nouvmdp.php

--- nouvmdp.php
+++ nouvmdp.php
@@ -1,6 +1,6 @@
 <?php 
 	session_start();
-	
+	$mail=$_POST['email'];
 	$Nmdp=$_POST['mdp1'];
 	$Nmdp2=$_POST['mdp2'];
 	if($Nmdp==$Nmdp2){
@@ -8,15 +8,15 @@
 		require "bd.php";
 		$bdd=getBD(); 
 		$rep= $bdd->prepare("SELECT * FROM utilisateurs WHERE mail = ?");
-		$rep->execute([$_SESSION['mail']]);
+		$rep->execute([$mail]);
 		$utl = $rep->fetch();   
 		if (!$utl) {
 			echo '<meta http-equiv="refresh" content="0;mdp_oublie.php"/>';
 		} else {
 			$rep= $bdd->prepare("UPDATE utilisateurs SET mdp = ? WHERE mail = ?");
-			$rep->execute([$Nmdp,$_SESSION['mail']]);
+			$rep->execute([$Nmdp,$mail]);
+			$_SESSION['mdp']==$Nmdp;
 			echo '<meta http-equiv="refresh" content="0;connecter.php"/>';
-			
 		}
 	} else {
 		echo '<meta http-equiv="refresh" content="0;mdp_oublie.php"/>';
