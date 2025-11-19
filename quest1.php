a556cb7c4d6dd92001c59d634164c36f6fa1a5e0
LeandreVSP <leandrevsp@gmail.com>
2025-11-19 15:01:06 +0100

    Update quest1.php

--- quest1.php
+++ quest1.php
@@ -1,9 +1,19 @@
 <?php
+	session_start();
 	$nom=$_POST['nom'];
 	$prenom=$_POST['prenom'];
 	$genre=$_POST['genre'];
-	require 'bd.php';
-	$bdd=getBD(); 
-	$rep= $bdd->prepare("UPDATE utilisateurs SET nom=?,prenom=?,genre=? WHERE mail=?");
-	$rep->execute([$nom,$prenom,$genre,$_SESSION['mail']]);
+	$date = str_replace("-", "/",$_POST['date']); 
+	echo $_SESSION['mail'];
+	if(is_string($nom)&&is_string($prenom)){
+		$_SESSION["nom"]=$nom;
+		$_SESSION["prenom"]=$prenom;
+		require 'bd.php';
+		$bdd=getBD(); 
+		$rep= $bdd->prepare("UPDATE utilisateurs SET nom=?,prenom=?,genre=?,date_naiss=? WHERE mail=?");
+		$rep->execute([$nom,$prenom,$genre,$date,$_SESSION['mail']]);
+		echo '<meta http-equiv="refresh" content="0;compte.php"/>';
+	} else{
+		echo '<meta http-equiv="refresh" content="0;questionnaire_nom.php"/>';
+	}
 ?>
