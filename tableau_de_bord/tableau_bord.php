0e46f589475ae11fd6c07dfc60204d08484afb1e
Nina <ndavalentin05@gmail.com>
2025-11-28 13:01:35 +0100

    esthétique tableau de bord

--- tableau_de_bord/tableau_bord.php
+++ tableau_de_bord/tableau_bord.php
@@ -32,15 +32,16 @@ foreach ($boulot as $row) {
 </head>
 <body>
     <?php include '../navbar.php'; ?>
-    <h1>Tableau de bord</h1>
-    <p>Suis ton évolution personnelle au cours du temps sur ce tableau de bord</p>
-        <h2> Graphiques d'évolution :</h2>
+    <h1 style='font-family: "Arial Rounded MT Bold", sans-serif;'>Tableau de bord</h1>
+    <p style='font-family: serif; font-size: 20px; color: grey; text-align : center; word-spacing: 4px;'>Suis ton évolution personnelle au cours du temps sur ce tableau de bord</p>
+    <br>
+    <h2 style='font-family: "Arial Rounded MT Bold", sans-serif; text-decoration: underline;'> Graphiques d'évolution :</h2>
 
 <div class="barre4">
     <h3>Graphique 1 : Evolution du sommeil</h3>
 
     <?php if (empty($duree_sommeil) || empty($dates)) : ?>
-        <div class="no-data-message">Il n'y a pas de données pour le moment.</div>
+        <div class="no-data-message" style='font-family: "Liberation Serif";'>Il n'y a pas de données pour le moment.</div>
     <?php else : ?>
         <canvas id="myChart1"></canvas>
     <?php endif; ?>
@@ -50,21 +51,21 @@ foreach ($boulot as $row) {
 <div class="barre4" style="margin-top: 80px;">
         <h3>Graphique 2 : Evolution du temps passé sur les réseaux sociaux</h3>
     <?php if (empty($duree_reseaux) || empty($dates)) : ?>
-        <div class="no-data-message">Il n'y a pas de données pour le moment.</div>
+        <div class="no-data-message" style='font-family: "Liberation Serif";'>Il n'y a pas de données pour le moment.</div>
     <?php endif; ?>
 <canvas id="myChart2"></canvas>
 
 
-    <h2>Résultats : </h2>
+    <h2 style='font-family: "Arial Rounded MT Bold", sans-serif; text-decoration: underline;'>Résultats : </h2>
 
-    <div class='bloc'> <a href='info_detaille.php'>Accéder aux details</a></div>
+    <div class='bloc' style='font-family: "Liberation Serif"'> <a href='info_detaille.php'>Accéder aux details</a></div>
     <ul>
         <li> Augmentation des notes</li>
         <li> Votre sommeil a évolué de ..%</li>
         <li> Point fort : </li>
         <li> Point faible : </li>
     </ul>
-    <h2>Améliorations à prévoire :</h2>
+    <h2 style='font-family: "Arial Rounded MT Bold", sans-serif; text-decoration: underline;'>Améliorations à prévoire :</h2>
     <ul>
         <li> Diminuer le temps passé sur le téléphone</li>
     </ul>
