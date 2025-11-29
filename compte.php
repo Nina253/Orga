537db6b775801e60ebb42ddafb9fd13cc973d277
Anthony253400 <mirandanthony2005@yahoo.fr>
2025-11-29 14:46:44 +0100

    chemin

--- compte.php
+++ compte.php
@@ -28,8 +28,8 @@
     <div class="content">
 
         <div class="left">
-            <div class='bloc'>  <a href="tableau_bord.php">Accéder à mon tableau de bord</a> </div>
-            <div class='bloc'>  <a href="questionnaire.php">Faire le questionnaire</a> </div>
+            <div class='bloc'>  <a href="tableau_de_bord/tableau_bord.php">Accéder à mon tableau de bord</a> </div>
+            <div class='bloc'>  <a href="questionnaire/questionnaire.php">Faire le questionnaire</a> </div>
             <div class='bloc'>  <a href="calendrier.php">Accéder à mon calendrier</a> </div>
             <div class='bloc'>  <a href="forum/forum.php">Accéder au forum</a> </div>
             <div class='bloc'> <a href="forum/mesSujets.php">Accéder à mes articles publiés dans le forum</a></div>
