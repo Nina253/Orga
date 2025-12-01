c770bb5d4ba4f92c6d8240950447adf177cb1f00
Leandre <leandrevsp@gmail.com>
2025-12-02 00:43:42 +0100

    reglage bug

--- questionnaire/quest2.php
+++ questionnaire/quest2.php
@@ -20,21 +20,22 @@
         $res=$rep->fetch();
         return $res['date_hab'];
     }
-    function ajouthab($bdd){
-        $rep2 = $bdd->prepare("INSERT INTO habitudes (id_etu, id_scolarite, id_hab) VALUES (?,?,?);");
-        $rep2 -> execute([$_SESSION['client']['id_etu'],$_SESSION['client']['id_scolarite'],$_SESSION['client']['id_hab']+1]);
+    function ajouthab($trav,$reso,$netf,$job,$dodo,$nutr,$sport,$ment,$para,$int,$bdd){
+        $rep2 = $bdd->prepare("INSERT INTO habitudes (duree_travail,duree_reseaux,duree_netflix,job,duree_sommeil,nutrition,freq_sport,sante_mentale,parascolaire,acces_internet,id_etu, id_scolarite, id_hab, date_hab) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,NOW());");
+        $rep2 -> execute([$trav,$reso,$netf,$job,$dodo,$nutr,$sport,$ment,$para,$int,$_SESSION['client']['id'],$_SESSION['client']['id_scolarite'],$_SESSION['client']['id_hab']+1]);
         $_SESSION['client']['id_hab']=$_SESSION['client']['id_hab']+1;
     }
-    function ajoutdate($bdd){
+    function ajoutdate($trav,$reso,$netf,$job,$dodo,$nutr,$sport,$ment,$para,$int,$bdd){
         $res=recupdate($bdd);
-        if ($res!=''){
-            ajouthab($bdd);
-        } else {
-            $rep= $bdd->prepare("UPDATE habitudes SET duree_travail=?,duree_reseaux=?,duree_netflix=?,job=?,duree_sommeil=?,nutrition=?,freq_sport=?,sante_mentale=?,parascolaire=?,acces_internet=?,date_hab=NOW() WHERE id_etu=? AND id_hab=");
+        if ($res===NULL){
+            $rep= $bdd->prepare("UPDATE habitudes SET duree_travail=?,duree_reseaux=?,duree_netflix=?,job=?,duree_sommeil=?,nutrition=?,freq_sport=?,sante_mentale=?,parascolaire=?,acces_internet=?,date_hab=NOW() WHERE id_etu=? AND id_hab=?");
             $rep->execute([$trav,$reso,$netf,$job,$dodo,$nutr,$sport,$ment,$para,$int,$_SESSION['client']['id'],$_SESSION['client']['id_hab']]);
+            
+        } else {
+            ajouthab($trav,$reso,$netf,$job,$dodo,$nutr,$sport,$ment,$para,$int,$bdd);
         }
     }
-    ajoutdate($bdd);
+    ajoutdate($trav,$reso,$netf,$job,$dodo,$nutr,$sport,$ment,$para,$int,$bdd);
     echo '<meta http-equiv="refresh" content="0;questionnaire_2.php"/>';
     
 
