b9b817cdf877113c6d06d5a195a69dbf59a32cfa
Leandre <leandrevsp@gmail.com>
2025-12-02 23:45:31 +0100

    Questionnaire fini 100% (NORMALEMENT)

--- questionnaire/quest1.php
+++ questionnaire/quest1.php
@@ -1,6 +1,14 @@
 
 <?php
 	session_start();
+	$date=$_POST['date'];
+	function age($date){
+		$naissance = new DateTime($date);
+		$aujourdHui = new DateTime();
+		$difference = $aujourdHui->diff($naissance);
+		return $difference->y;
+	}
+	
 	$nom=$_POST['nom'];
 	$prenom=$_POST['prenom'];
 	$genre=$_POST['genre'];
@@ -10,8 +18,8 @@
 		$_SESSION["prenom"]=$prenom;
 		require '../bd.php';
 		$bdd=getBD(); 
-		$rep= $bdd->prepare("UPDATE etudiant SET nom=?,prenom=?,genre=?,niveau_educ_parents=? WHERE mail=?");
-		$rep->execute([$nom,$prenom,$genre,$niv,$_SESSION['mail']]);
+		$rep= $bdd->prepare("UPDATE etudiant SET nom=?,prenom=?,age=?,genre=?,niveau_educ_parents=? WHERE mail=?");
+		$rep->execute([$nom,$prenom,age($date),$genre,$niv,$_SESSION['mail']]);
 		echo '<meta http-equiv="refresh" content="0;../compte.php"/>';
 	} else{
 		echo '<meta http-equiv="refresh" content="0;questionnaire_nom.php"/>';
--- questionnaire/quest_3.php
+++ questionnaire/quest_3.php
@@ -5,10 +5,43 @@
     $moy = ($moyenne/20)*100;
 	
     require '../bd.php';
-    $bdd=getBD(); 
-    $rep= $bdd->prepare("UPDATE scolarite SET presence=?,notes=? WHERE id_etu=?");
-    $rep->execute([$pres,$moy,$_SESSION['client']['id']]);
-    echo '<meta http-equiv="refresh" content="0;../home.php"/>';
+    $bdd=getBD();
+    function recupdate($bdd){
+        $rep= $bdd->prepare("SELECT date_scolarite FROM scolarite WHERE id_etu=?");
+        $rep->execute([$_SESSION['client']['id']]);
+        $res=$rep->fetch();
+        return $res['date_scolarite'];
+    }
+
+    function recupidsco($bdd){
+        $rep= $bdd->prepare("SELECT id_scolarite FROM scolarite ORDER BY id_scolarite DESC LIMIT 1");
+        $rep->execute([]);
+        $res=$rep->fetch();
+        return $res['id_scolarite'];
+    }
     
+    
+    function ajoutsco($pres,$moy,$bdd){
+        $resid=recupidsco($bdd);
+        $rep2 = $bdd->prepare("INSERT INTO scolarite (id_etu,id_scolarite,presence,notes,date_scolarite) VALUES (?,?,?,?,YEAR(NOW()));");
+        $rep2 -> execute([$_SESSION['client']['id'],$resid+1,$pres,$moy]);
+        $_SESSION['client']['id_scolarite']=$resid+1;
+    }
+
+    function ajoutdate($pres,$moy,$bdd){
+        $res=recupdate($bdd);
+        if ($res===NULL){
+            $rep= $bdd->prepare("UPDATE scolarite SET presence=?,notes=?,date_scolarite=YEAR(NOW()) WHERE id_etu=? AND id_scolarite=?");
+            $rep->execute([$pres,$moy,$_SESSION['client']['id'],$_SESSION['client']['id_scolarite']]);
+            
+        } else {
+            ajoutsco($pres,$moy,$bdd);
+        }
+    }
+
+    
+    ajoutdate($pres,$moy,$bdd);
+    echo '<meta http-equiv="refresh" content="0;../home.php"/>';
+
 
 ?>
