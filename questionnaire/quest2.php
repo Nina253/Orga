037635aa0d08b98b0398bce0f8c97de8a135af28
Leandre <leandrevsp@gmail.com>
2025-12-02 17:09:21 +0100

    reglage bug

--- questionnaire/quest2.php
+++ questionnaire/quest2.php
@@ -20,10 +20,17 @@
         $res=$rep->fetch();
         return $res['date_hab'];
     }
+    function recupidhab($bdd){
+        $rep= $bdd->prepare("SELECT id_hab FROM habitudes ORDER BY id_hab DESC LIMIT 1");
+        $rep->execute([]);
+        $res=$rep->fetch();
+        return $res['id_hab'];
+    }
     function ajouthab($trav,$reso,$netf,$job,$dodo,$nutr,$sport,$ment,$para,$int,$bdd){
+        $resid=recupidhab($bdd);
         $rep2 = $bdd->prepare("INSERT INTO habitudes (duree_travail,duree_reseaux,duree_netflix,job,duree_sommeil,nutrition,freq_sport,sante_mentale,parascolaire,acces_internet,id_etu, id_scolarite, id_hab, date_hab) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,NOW());");
-        $rep2 -> execute([$trav,$reso,$netf,$job,$dodo,$nutr,$sport,$ment,$para,$int,$_SESSION['client']['id'],$_SESSION['client']['id_scolarite'],$_SESSION['client']['id_hab']+1]);
-        $_SESSION['client']['id_hab']=$_SESSION['client']['id_hab']+1;
+        $rep2 -> execute([$trav,$reso,$netf,$job,$dodo,$nutr,$sport,$ment,$para,$int,$_SESSION['client']['id'],$_SESSION['client']['id_scolarite'],$resid+1]);
+        $_SESSION['client']['id_hab']=$resid+1;
     }
     function ajoutdate($trav,$reso,$netf,$job,$dodo,$nutr,$sport,$ment,$para,$int,$bdd){
         $res=recupdate($bdd);
